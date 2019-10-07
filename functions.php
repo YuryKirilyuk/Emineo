1d91279d097022a70d9848accaded9037db05299
Yury Kirilyuk <yury@cyberiansoft.com>
2019-10-07 21:16:03 +0300

    services page

--- functions.php
+++ functions.php
@@ -159,59 +159,189 @@ function process_jobs_feed($url){
     }
     if(!empty($feeds)){
         $job_details = array();
-        $job_details['Job Id'] = htmlspecialchars($feeds->{'vacancies'}->{'PosID'});
-        $job_details['Last Modified'] = htmlspecialchars($feeds->{'vacancies'}->{'LastModified'});
-        $job_details['HR Manager'] = htmlspecialchars($feeds->{'vacancies'}->{'Verantwortlich_HR'});
-        $job_details['Job Details'] = array(
-            'Title of job' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'Stellentitel'}),
-            'Supplement to job title' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'Zusatz_Stellentitel'}),
-            'Title of tasks' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'TitelAufgaben'}),
-            'Text for tasks' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'TextAufgaben'}),
-            'Title of requirements' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'TitelAnforderungen'}),
-            'Text for requirements' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'TextAnforderungen'}),
-            'Title What we offer' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'TitelWirbieten'}),
-            'Text What we offer' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'TextWirbieten'}),
-            'Outro' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'Abspann'}),
-            'Short description' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}->{'Kurzbeschreibung'})
-        );
-        $job_details['Criteria'] = array(
-            'Type of employment' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}->{'Beschäftigungsart'}),
-            'Terminability' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}->{'Befristung'}),
-            'Work as' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}->{'EinstiegAls'}),
-            'Deparment' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}->{'Unternehmensbereich'}),
-            'Work location' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}->{'Arbeitsort'}),
-        );
-        $job_details['Choice of image'] = htmlspecialchars($feeds->{'vacancies'}->{'Bildwahl'});
-        $job_details['Layout Design'] = htmlspecialchars($feeds->{'vacancies'}->{'Inserate_Layout'});
-        $job_details['Application URL'] = htmlspecialchars($feeds->{'vacancies'}->{'Apply_url'});
+        $i = 0;
+        foreach($feeds->vacancies->PosID as $pos_id) {
+            $single_job = [];
+            $single_job['posid'] = $pos_id;
+            $single_job['Job Id'] = htmlspecialchars($pos_id);
+            $single_job['Last Modified'] = htmlspecialchars($feeds->{'vacancies'}->{'LastModified'}[$i]);
+            $single_job['HR Manager'] = htmlspecialchars($feeds->{'vacancies'}->{'Verantwortlich_HR'}[$i]);
+            $single_job['Job Details'] = array(
+                'Title of job' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'Stellentitel'}),
+                'Supplement to job title' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'Zusatz_Stellentitel'}),
+                'Title of tasks' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'TitelAufgaben'}),
+                'Text for tasks' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'TextAufgaben'}),
+                'Title of requirements' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'TitelAnforderungen'}),
+                'Text for requirements' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'TextAnforderungen'}),
+                'Title What we offer' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'TitelWirbieten'}),
+                'Text What we offer' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'TextWirbieten'}),
+                'Outro' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'Abspann'}),
+                'Short description' => htmlspecialchars($feeds->{'vacancies'}->{'Ausschreibungstext'}[$i]->{'Kurzbeschreibung'})
+            );
+            $single_job['Criteria'] = array(
+                'Type of employment' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}[$i]->{'Beschäftigungsart'}),
+                'Terminability' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}[$i]->{'Befristung'}),
+                'Work as' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}[$i]->{'EinstiegAls'}),
+                'Deparment' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}[$i]->{'Unternehmensbereich'}),
+                'Work location' => htmlspecialchars($feeds->{'vacancies'}->{'Suchkriterien'}[$i]->{'Arbeitsort'}),
+            );
+            $single_job['Choice of image'] = htmlspecialchars($feeds->{'vacancies'}->{'Bildwahl'}[$i]);
+            $single_job['Layout Design'] = htmlspecialchars($feeds->{'vacancies'}->{'Inserate_Layout'}[$i]);
+            $single_job['Application URL'] = htmlspecialchars($feeds->{'vacancies'}->{'Apply_url'}[$i]);
+            $job_details[] = $single_job;
+            $i++;
+        }
         return $job_details;
     }
-    return '';
+    return [];
 }
-
 /*
-add_action('wp', function(){
-    if(is_page('home')){
-        $url = 'https://recruitingapp-2895.umantis.com/XMLExport/133';
-        $jobs = process_jobs_feed($url);
-        echo '<pre>';print_r($jobs);echo '</pre>';
-    }
-});
+ add_action('wp', function(){
+ 	if(is_page('jobs-feed')){
+ 		$url = 'https://recruitingapp-2895.umantis.com/XMLExport/133';
+ 		$jobs = process_jobs_feed($url);
+ 		echo '<pre>';print_r($jobs);echo '</pre>';
+ 	}
+ });
 */
 
+function Generate_job($atts) {
 
-function Generate_job() {
+    $a = shortcode_atts( array(
+        'howmany' => '-1',
+        'offset' => '0',
+        'mode' => 'light',
+        'posid' => '',
+        'exclude_posid' => ''
+    ), $atts );
 
     $url = 'https://recruitingapp-2895.umantis.com/XMLExport/133';
     $jobs = process_jobs_feed($url);
+    $out = '';
+    if($a['mode'] == 'light') {
+        $out = '<div class="elementor-row1 jobs-list-main-container">';
+    }
 
-    $out = '
-	<div class="job-category">_' . $jobs['Criteria']['Deparment'] . '</div>
-    <h3 class="job-title">' . $jobs['Job Details']['Title of job'] . '</h3>
-    <div class="job-location">' . $jobs['Criteria']['Work location'] . '</div>
-    <div class="job-description">' . $jobs['Job Details']['Short description'] . '</div>
-    <a class="btn-link" href="' . $jobs['Application URL'] . '">Mehr Erfahren</a>
-	';
+    $posids = [];
+    if($a['posid']) {
+        $posids = explode(",", $a['posid']);
+    }
+
+    if($posids) {
+        $splitted_jobs = [];
+        if($jobs) {
+            foreach($jobs as $job) {
+                if(in_array($job['posid'], $posids)) {
+                    $splitted_jobs[] = $job;
+                }
+            }
+        }
+        $jobs = $splitted_jobs;
+    }
+    else {
+        if($a['howmany'] == '-1') {
+            $jobs = array_slice($jobs, $a['offset']);
+        }
+        else {
+            $jobs = array_slice($jobs, $a['offset'], $a['howmany']);
+        }
+    }
+
+
+    if($a['exclude_posid']) {
+        $splitted_jobs = [];
+        $ex_posids = explode(",", $a['exclude_posid']);
+        if($jobs) {
+            foreach($jobs as $job) {
+                if(!in_array($job['posid'], $ex_posids)) {
+                    $splitted_jobs[] = $job;
+                }
+            }
+        }
+        $jobs = $splitted_jobs;
+    }
+
+    if($jobs) {
+        foreach($jobs as $job) {
+            if($a['mode'] == 'light') {
+                $out .= '
+				<div class="single-job-col-4">
+					<div class="single-job-container">
+						<div class="single-job-inner-container">
+				';
+            }
+
+            $out .= '
+				<div class="job-category">_' . $job['Criteria']['Deparment'] . '</div>
+				<h3 class="job-title">' . $job['Job Details']['Title of job'] . '</h3>
+				<div class="job-location">' . $job['Criteria']['Work location'] . '</div>
+				<div class="job-description">' . $job['Job Details']['Short description'] . '</div>
+				<a class="btn-link" target="_blank" href="' . $job['Application URL'] . '">Mehr Erfahren</a>
+			';
+
+            if($a['mode'] == 'light') {
+                $out .= '
+						</div>
+					</div>
+				</div>
+				';
+            }
+        }
+
+        if($a['mode'] == 'light') {
+            $out .= '
+		</div>
+
+		<style>
+		.jobs-list-main-container .single-job-col-4 {
+			float: left;
+			width: 33.33%;
+			/* display: table-cell; */
+			height: 100%;
+		}
+		
+		.jobs-list-main-container .single-job-col-4:nth-child(3n + 1) {
+			clear: both;
+		}
+		
+		.jobs-list-main-container .single-job-container {
+			padding: 10px;
+		}
+		
+		.jobs-list-main-container .single-job-col-4:nth-child(3n + 1) .single-job-container {
+			padding-left: 0;
+		}
+		
+		.jobs-list-main-container .single-job-col-4:nth-child(3n) .single-job-container {
+			padding-right: 0;
+		}
+		
+		.jobs-list-main-container .single-job-inner-container {
+			background-color: #EFF1F4;
+			padding: 25px;
+		}
+		
+		.jobs-list-main-container {
+			margin-top: 30px;
+			display: inline-block;
+		}
+
+		@media(max-width: 480px) {
+			.jobs-list-main-container .single-job-col-4 {
+				width: 100%;
+			}
+			.jobs-list-main-container .single-job-col-4:nth-child(3n + 1) .single-job-container {
+				padding-left: 10px;
+			}
+			
+			.jobs-list-main-container .single-job-col-4:nth-child(3n) .single-job-container {
+				padding-right: 10px;
+			}
+		}
+		</style>
+		';
+        }
+    }
 
     return $out;
 }
