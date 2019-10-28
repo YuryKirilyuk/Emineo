513152a03c3a6304d5abb41c669716be585ddf41
Yury Kirilyuk <yury@cyberiansoft.com>
2019-10-28 23:44:58 +0200

    contact map, testimonial slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -53,29 +53,32 @@ jQuery(function($){
 
 
     //swiper on Karriere page
-    var swiper = new Swiper('.karriere .swiper-container', {
-        slidesPerView: 2,
-        slidesPerColumn: 2,
-        spaceBetween: 30,
-        breakpoints: {
-            580: {
-                autoHeight: true,
-                slidesPerView: 1,
-                slidesPerColumn: 1
-            }
-        },
-        pagination: {
-            el: '.karriere .swiper-pagination',
-            clickable: true,
-            renderBullet: function (index, className) {
-                return '<span class="' + className + '">' + (index + 1) + '</span>';
+    if($('.jobs-list').length) {
+        var swiper = new Swiper('.karriere .swiper-container', {
+            slidesPerView: 2,
+            slidesPerColumn: 2,
+            spaceBetween: 30,
+            breakpoints: {
+                580: {
+                    autoHeight: true,
+                    slidesPerView: 1,
+                    slidesPerColumn: 1
+                }
             },
-        },
-        navigation: {
-            nextEl: '.karriere .swiper-button-next',
-            prevEl: '.karriere .swiper-button-prev',
-        },
-    });
+            pagination: {
+                el: '.karriere .swiper-pagination',
+                clickable: true,
+                renderBullet: function (index, className) {
+                    return '<span class="' + className + '">' + (index + 1) + '</span>';
+                },
+            },
+            navigation: {
+                nextEl: '.karriere .swiper-button-next',
+                prevEl: '.karriere .swiper-button-prev',
+            },
+        });
+    }
+
 
     var swiper_2 = new Swiper('.elementor-swiper .swiper-container', {
         slidesPerView: 'auto',
